5c59ed4dff670d01966f40bde2c382a09d571b82
zhangjun <5024064@qq.com>
2017-12-19 21:29:53 +0800

    跳转修改：根路径跳转到webroot的index.php

--- index.php
+++ index.php
@@ -1,3 +1,2 @@
 <?php
-require __DIR__. '/vendor/autoload.php';
-include('layout/website.php');
+require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
---
+++ webroot/index.php
@@ -0,0 +1,4 @@
+<?php
+require dirname(__DIR__). '/vendor/autoload.php';
+include(dirname(__DIR__).'config/config.php');
+include(dirname(__DIR__).'layout/website.php');
