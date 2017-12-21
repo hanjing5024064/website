4f912acaf74d8ef88164366c92f232ccce0dd1e8
zhangjun <5024064@qq.com>
2017-12-21 22:07:12 +0800

    change config of particlesjs

--- element/footer.php
+++ element/footer.php
@@ -45,7 +45,7 @@
 <script>
     $(function(){
         /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-        particlesJS.load('particles-js', 'assets/particles.json', function() {
+        particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
             console.log('callback - particles.js config loaded');
         });
     });
