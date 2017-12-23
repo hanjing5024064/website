16cee18efd8f4f3b0bf56621d3c11f03472f6027
zhangjun <5024064@qq.com>
2017-12-23 11:03:27 +0800

    微信页面图片轮播-增加3张图

--- pages/wechat.php
+++ pages/wechat.php
@@ -1,11 +1,11 @@
 <div class="row">
     <div class="col-lg-12 text-center">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
+<!--            <ol class="carousel-indicators">-->
+<!--                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>-->
+<!--                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>-->
+<!--                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>-->
+<!--            </ol>-->
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <img class="img-fluid" src="img/wechat/1.png" alt="First slide">
@@ -14,7 +14,13 @@
                     <img class="img-fluid" src="img/wechat/2.png" alt="Second slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="img-fluid" src="img/wechat/2.png" alt="Third slide">
+                    <img class="img-fluid" src="img/wechat/3.png" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="img-fluid" src="img/wechat/4.png" alt="Third slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="img-fluid" src="img/wechat/4.png" alt="Third slide">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
