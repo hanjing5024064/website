c5da1de4f92fca5bd0ab07016ef8f28788741d7d
zhangjun <5024064@qq.com>
2017-12-28 10:58:22 +0800

    add wechat image

--- pages/wechat.php
+++ pages/wechat.php
@@ -9,6 +9,19 @@
                 <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/2.png"/></div>
                 <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/4.png"/></div>
                 <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/1.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/5.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/6.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/7.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/8.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/9.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/10.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/11.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/12.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/13.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/14.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/15.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/16.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/17.png"/></div>
             </div>
             <!-- If we need pagination -->
             <div class="swiper-pagination"></div>
