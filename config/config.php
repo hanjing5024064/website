91a5dc862f0e67348da2fc8d6ce64a6b9ff3d6a9
zhangjun <5024064@qq.com>
2017-12-28 09:25:34 +0800

    change qrcode

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 $companyFullName = "上海鸿维信息技术有限公司";
 $companyShortName = "上海鸿维";
-$qrcodeImg = 'qrcode_ytzh.jpg';
+$qrcodeImg = 'qrcode_hwwl.jpg';
 $icpCode = '沪ICP备17006400号-1';
 $policeCode = '';
 $contactQQ = '79811993';
