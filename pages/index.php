287245f6980e198950faaa1cca856379d78e5d22
zhangjun <5024064@qq.com>
2018-05-09 16:18:52 +0800

    首页swiper显示前进、后退按钮

--- pages/index.php
+++ pages/index.php
@@ -122,8 +122,8 @@
         <div class="swiper-pagination"></div>
 
         <!-- If we need navigation buttons -->
-        <!--    <div class="swiper-button-prev"></div>-->
-        <!--    <div class="swiper-button-next"></div>-->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
 
         <!-- If we need scrollbar -->
         <!--    <div class="swiper-scrollbar"></div>-->
