3377047b1339acb09d17db73ae8de086ba511fd7
zhangjun <5024064@qq.com>
2018-05-09 16:19:33 +0800

    图片链接到阿里的对象存储服务中，通过CDN加速，效果杠杠的：）

--- pages/wechat.php
+++ pages/wechat.php
@@ -5,23 +5,23 @@
             <!-- Additional required wrapper -->
             <div class="swiper-wrapper">
                 <!-- Slides -->
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_0.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_1.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_2.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_3.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_4.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_5.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_6.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_7.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_8.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_9.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_10.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/ytzh_11.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/3.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/2.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/4.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/1.png"/></div>
-                <div class="swiper-slide swiper-slide-wechat"><img src="img/wechat/5.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_0.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_1.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_2.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_3.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_4.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_5.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_6.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_7.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_8.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_9.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_10.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/ytzh_11.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/3.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/2.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/4.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/1.png"/></div>
+                <div class="swiper-slide swiper-slide-wechat"><img src="http://img.jyfrl.com/jyfrl/img/wechat/5.png"/></div>
             </div>
             <!-- If we need pagination -->
 <!--            <div class="swiper-pagination"></div>-->
